132d2d018e0b82c65bd38e7e81f966d8b5130782
suiyujin <suiyujin04@gmail.com>
2015-10-19 02:18:34 +0900

    settings for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: '210.140.71.3', port: 80 }
 end
