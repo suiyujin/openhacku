63b4f2fd2b1c74aff7235a837b7c67a7a3c4147c
suiyujin <suiyujin04@gmail.com>
2015-10-19 02:24:07 +0900

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018164935) do
+ActiveRecord::Schema.define(version: 20151018172137) do
+
+  create_table "requests", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
