c8d52641e5471847a41bc2bb16aa00a0c8a88dee
suiyujin <suiyujin04@gmail.com>
2015-10-21 17:38:37 +0900

    enable params for ordering

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   belongs_to :bought_user, class_name: 'User'
 
-  scope :order_limit_offset, ->(limit, offset) { order(id: :desc).limit(limit).offset(offset) }
+  scope :order_limit_offset, ->(order, limit, offset) { order(order).limit(limit).offset(offset) }
 end
