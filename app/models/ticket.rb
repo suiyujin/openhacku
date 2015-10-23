be379773f073c85f6ad51a6a90c53a7c1932359f
suiyujin <suiyujin04@gmail.com>
2015-10-24 01:34:19 +0900

    except no bought tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   belongs_to :bought_user, class_name: 'User'
 
+  scope :bought, -> { where(bought: true) }
+  scope :no_bought, -> { where(bought: false) }
   scope :order_limit_offset, ->(order, limit, offset) { order(order).limit(limit).offset(offset) }
 end
