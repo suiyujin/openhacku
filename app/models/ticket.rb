b68dcc769c10cb0e43db4fc27dd5e7d4a6dfcc28
suiyujin <suiyujin04@gmail.com>
2015-10-24 01:54:37 +0900

    define scope of user and bought_user

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,8 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   belongs_to :bought_user, class_name: 'User'
 
+  scope :user, ->(user_id) { where(user_id: user_id) }
+  scope :bought_user, ->(bought_user_id) { where(bought_user_id: bought_user_id) }
   scope :bought, -> { where(bought: true) }
   scope :no_bought, -> { where(bought: false) }
   scope :order_limit_offset, ->(order, limit, offset) { order(order).limit(limit).offset(offset) }
