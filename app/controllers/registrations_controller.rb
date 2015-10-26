bc230a177bebdbd58f67e9bf860fa2bca1e283d1
suiyujin <suiyujin04@gmail.com>
2015-10-26 16:28:45 +0900

    create keywords_users after saving user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    super do |resource|
+      # create keywords_users
+      keywords = params[:user][:keywords].map { |keyword_id| Hash[*['keyword_id', 'user_id'].zip([keyword_id, resource.id]).flatten] }
+      KeywordsUser.create(keywords)
+    end
+  end
+
   def respond_with(resource, opts = {})
     render json: {
       id: resource.id,
