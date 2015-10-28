f776dbe0bcddce9b9695a2977ef9e9de04a588fa
suiyujin <suiyujin04@gmail.com>
2015-10-29 00:06:52 +0900

    add begginer to tickets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028085334) do
+ActiveRecord::Schema.define(version: 20151028150525) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", limit: 255, null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20151028085334) do
     t.integer  "bought_user_id", limit: 4
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
+    t.boolean  "beginner",                     default: false, null: false
   end
 
   add_index "tickets", ["bought_user_id"], name: "index_tickets_on_bought_user_id", using: :btree
