765d04e34b687a3ba08d427184c0c5932d5b98e5
suiyujin <suiyujin04@gmail.com>
2015-10-29 19:23:40 +0900

    register user without params of user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
-  def create
-    super do |resource|
-      # create keywords_users
-      keywords = params[:user][:keywords].map { |keyword_id| Hash[*['keyword_id', 'user_id'].zip([keyword_id, resource.id]).flatten] }
-      KeywordsUser.create(keywords)
-    end
-  end
-
   def respond_with(resource, opts = {})
     render json: {
       id: resource.id,
-      email: resource.email,
-      name: resource.name,
-      introduction: resource.introduction,
-      keywords: resource.keywords
+      email: resource.email
     }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029060803) do
+ActiveRecord::Schema.define(version: 20151029101850) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", limit: 255, null: false
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 20151029060803) do
     t.string   "authentication_token",   limit: 255
     t.datetime "created_at",                                                             null: false
     t.datetime "updated_at",                                                             null: false
-    t.string   "name",                   limit: 255,                                     null: false
+    t.string   "name",                   limit: 255
     t.string   "introduction",           limit: 255
     t.string   "profile_img_url",        limit: 255, default: "image/dummy_profile.jpg", null: false
     t.string   "header_img_url",         limit: 255, default: "image/dummy_header.jpg",  null: false
