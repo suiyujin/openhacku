92147182c651616c8290952202b882d7399aaa32
suiyujin <suiyujin04@gmail.com>
2015-10-29 23:30:59 +0900

    allow cross domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # allow cross domain
+  before_filter :allow_cross_domain_access
+  def allow_cross_domain_access
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+  end
+
   # json でのリクエストの場合CSRFトークンの検証をスキップ
   skip_before_action :verify_authenticity_token, if: -> {request.format.json?}
   # トークンによる認証
