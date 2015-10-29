c1b8bc1d03f6897c4508997e1581ae78625bf413
suiyujin <suiyujin04@gmail.com>
2015-10-29 23:56:20 +0900

    except csrf protect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   # allow cross domain
   before_filter :allow_cross_domain_access
