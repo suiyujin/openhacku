623557c4a62de604b69ea57ba59699e08903faec
suiyujin <suiyujin04@gmail.com>
2015-11-03 08:58:08 +0900

    fix request format not json in login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
   def create
     super do |resource|
-      resource.ensure_authentication_token if request.format.json?
+      resource.ensure_authentication_token
     end
   end
 
