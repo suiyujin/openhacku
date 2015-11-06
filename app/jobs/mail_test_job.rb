fc3ee9229eb5a084c3f095641c0a89f1b8ef0d39
suiyujin <suiyujin04@gmail.com>
2015-11-07 05:11:27 +0900

    use test_mail

--- app/jobs/mail_test_job.rb
+++ app/jobs/mail_test_job.rb
@@ -1,11 +1,12 @@
 class MailTestJob < ActiveJob::Base
-  queue_as :default
+  queue_as :email
 
   def perform
-    matching_user_ids = [1,8,9]
-    ticket = Ticket.find(94)
-    # お互いに通知する
-    MatchingMailer.for_matching_user(matching_user_ids, ticket).deliver_later
-    # MatchingMailer.for_creating_user(matching_user_ids, ticket).deliver_now
+    MatchingMailer.test_mail.deliver_now
+   # matching_user_ids = [1,8,9]
+   # ticket = Ticket.find(94)
+   # # お互いに通知する
+   # MatchingMailer.for_matching_user(matching_user_ids, ticket).deliver_now
+   # # MatchingMailer.for_creating_user(matching_user_ids, ticket).deliver_now
   end
 end
--- app/mailers/matching_mailer.rb
+++ app/mailers/matching_mailer.rb
@@ -1,4 +1,5 @@
 class MatchingMailer < ApplicationMailer
+  default from: "from@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
