e5cb141a09c81dc606bce867f1adf256b1ed8864
suiyujin <suiyujin04@gmail.com>
2015-11-07 10:37:39 +0900

    add more more dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,42 +28,9 @@ users = CSV.read('db/dummy_data/users.csv')
 User.create(make_dummy_data(users))
 p 'users created.'
 
-# sample tickets
-dummy_tickets = Array.new
-1.upto(users.size-1) do |user_id|
-  1.upto(5) do |num|
-    dummy_tickets << {
-      title: "title#{user_id}#{num}",
-      body: "body#{user_id}#{num}",
-      time: [0.5, 1.0, 1.5, 2.0, 2.5, 3.0].sample,
-      price: [300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500].sample,
-      skype: [true, false].sample,
-      hangouts: [true, false].sample,
-      offline_place: ["新宿", "渋谷", "つくば"].sample,
-      user_id: user_id,
-      beginner: [true, false].sample
-    }
-  end
-end
-Ticket.create(dummy_tickets)
-p 'sample tickets created.'
-
 # tickets
 Ticket.create(make_dummy_data(CSV.read('db/dummy_data/tickets.csv')))
 p 'tickets created.'
-
-# sample keywords_tickets
-dummy_keywords_tickets = Array.new
-1.upto((users.size-1)*5) do |ticket_id|
-  1.upto(5) do |num|
-    dummy_keywords_tickets << {
-      ticket_id: ticket_id,
-      keyword_id: num
-    }
-  end
-end
-KeywordsTicket.create(dummy_keywords_tickets)
-p 'sample keywords_tickets created.'
 
 # keywords_tickets
 KeywordsTicket.create(make_dummy_data(CSV.read('db/dummy_data/keywords_tickets.csv')))
@@ -100,19 +67,6 @@ end
 StockTicket.create(dummy_stock_tickets)
 p 'sample stock_tickets created.'
 
-# sample ticket_candidates
-dummy_ticket_candidates = Array.new
-1.upto(users.size-1) do |user_id|
-  1.upto(5) do |num|
-    dummy_ticket_candidates << {
-      user_id: user_id,
-      ticket_id: (5 * user_id) + num,
-      comment: "教えます"
-    }
-  end
-end
-TicketCandidate.create(dummy_ticket_candidates)
-p 'sample ticket_candidates created.'
 
 # ticket_candidates
 TicketCandidate.create(make_dummy_data(CSV.read('db/dummy_data/ticket_candidates.csv')))
